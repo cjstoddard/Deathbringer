03d269984c468379003f0f8a00a7de7ba7c4ca5b
cjstoddard <chris.j.stoddard@gmail.com>
2023-08-07 20:43:46 -0500

    new file:   Example.txt modified:   README.md deleted:    binaries/deathbringer deleted:    binaries/deathbringer.exe deleted:    binaries/qb64-deathbringer.exe new file:   deathbringer.py

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # Deathbringer
 
-This program randomly generates a character for the Deathbringer RPG, the full RPG is availble at;
+These programs randomly generate a character for the Deathbringer RPG. Both these programs are commandline, text only programs, there is no fancy Windows GUI, this will not change. This is an offshoot project from a Basic program I wrote in the early 90's for making D&D characters.
+
+ The Deathbringer RPG is availble at;
 
 https://www.drivethrurpg.com/product/396879/Deathbringer-RPG?term=deathbringer
 
-This program differs from the rules in that it does not use point buy, but rather generates attributes randomly by rolling 4d6 and taking the best three, and using that number to determine the actual attribute ranging from -4 to +4. I may add the point buy system as an option later.
+Deathbringer.bas:
+
+I am no longer updating this code, all future updates will be done on deathbringer.py. This code differs from the rules in that it does not use point buy, but rather generates attributes randomly by rolling 4d6 and taking the best three, and using that number to determine the actual attribute ranging from -4 to +4.
 
 Compiling:
 This program was writen for Qbasic and can be compiled with QB64 or FreeBasic;
@@ -13,20 +17,23 @@ FreeBasic is available at https://www.freebasic.net/. To compile under FreeBasic
 
 fbc -lang qb deathbringer.bas -x binaries/deathbringer 
 
-QB64 is availble from https://qb64.com/. If you are using QB64 or have downloaded Qbasic from the internet, the first line of the program must be changed from Randomize (Timer, 3) to Randomize Timer. To compile under QB64, use the folowing;
+I am aware this is really bad code. I originally wrote this program back in the early 90's to roll up D&D characters. While I am capable of cleaning it up and making it into a decent program, I choose not to, I like the old school coding because it goes along with the old school feel of the Deathbringer RPG. 
 
-qb64 -c deathbringer.bas
+Deathbringer.py: This program does use the point buy system used in the RPG rules. I wrote this program with Python 3.11.2, it should work fine with any 3.x version, I will probably ignore the problem if it doesn't work on versions of Python other than what I am using. You will need Python to run this program, Python is free and open software, it is availble for download at Python.org. If you are not familar with Python, here is a quick guide on getting it installed in Windows. If you use Linux or Mac OS, it is already installed.
 
-Notes:
+https://www.howtogeek.com/197947/how-to-install-python-on-windows/
 
-I am aware this is really bad code. I originally wrote this program back in the early 90's to roll up D&D characters. While I am capable of cleaning it up and making it into a decent program, I choose not to, I like the old school coding because it goes along with the old school feel of the Deathbringer RPG.
+If you are completely unfamilar with Python, it is a fairly easy programing language to learn. There are many good tutorials on the internet for learning the basics of Python. Below is an hour long Youtube video that gives you pretty much everything you need to know about Python to understand how this program works so you can start modifing it for you own purposes. One of my goals here was to build a simple enough code base where even someone with no Python experience can add thier own Classes, backgrounds or Miseries just by looking at the code and doing a bit of copy/paste and modify work. I encourage everyone to copy this program, modify it and make it your own.
 
-At some point I will add pre compiled Windows and Linux binaries for those who do not want to go to the trouble of downloading and installing QB64 or FreeBasic. Sorry OS X users, I do not own a Mac, you are on your own.
+https://www.youtube.com/watch?v=kqtD5dpn9C8&t=1882s
 
-Thank you to Professor Dungeon Master for creating the Deathbringer RPG, your hard work and creativity is appreciated.
+Thank you to Dan "Professor Dungeon Master" Masters for creating the Deathbringer RPG, your hard work and creativity is appreciated.
 
 https://www.patreon.com/DungeonCraftYouTube/posts
 
 https://www.youtube.com/@DUNGEONCRAFT1
+
+Legal Statement:
+This Deathbringer Character Generator is an independent product and is not affiliated with Dan "Professor Dungeon Master" Masters. Deathbringer RPG © 2023 by Dan "Professor Dungeon Master" Masters.
 
 Disclaimer: This software is provided "AS IS", without warranty of any kind, express or implied, including but not limited to warranties of merchantability, fitness for a paticular purpose and nonifringment. In no event shall the author or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the software.
---
+++ deathbringer.py
@@ -0,0 +1,265 @@
+#' ************************************
+#' ** Deathbringer.py 1.1            **
+#' ** By Chris Stoddard, March 2023  **
+#' ** Based on the Deathbringer RPG  **
+#' ** by Professor Dungeon Master    **
+#' ************************************
+
+###############################
+# Import needed libraries
+import random
+
+###############################
+# Define functions
+
+###############################
+# Print Legal Statement.
+print ("------------------------------------------------------------")
+print ("Legal Statement:")
+print ("This Deathbringer Character Generator is an independent")
+print ("product and is not affiliated with Professor Dungeon Master.")
+print ("Deathbringer RPG © 2023 by Professor Dungeon Master.")
+print ("------------------------------------------------------------")
+
+# Assign ability scores
+print ('\n')
+print ("You have 8 build points to distribute between your 6 ability scores.")
+print ("Strength, Dexerity, Constitution, Intellegence, Wisdom and Charisma.")
+print ("No single ability score can exceed +6")
+
+Build_Points = 8
+
+print ("You Have " + str(Build_Points) + " to spend.")
+
+Strength = input ("How many build points do you want to assign to Strength? ")
+if Build_Points == 0:
+    Strength = "0"
+if int(Strength) > 6:
+    Strength = "6"
+if int(Strength) < 0:
+    Strength = "0"
+if int(Strength) > Build_Points:
+    Strength = str(Build_Points)
+Build_Points = (Build_Points - int(Strength))
+print ("You Have " + str(Build_Points) + " to spend.")
+
+Dexterity = input ("How many build points do you want to assign to Dexterity? ")
+if Build_Points == 0:
+    Dexterity = "0"
+if int(Dexterity) > 6:
+    Dexterity = "6"
+if int(Dexterity) < 0:
+    Dexterity = "0"
+if int(Dexterity) > Build_Points:
+    Dexterity = str(Build_Points)
+Build_Points = (Build_Points - int(Dexterity))
+print ("You Have " + str(Build_Points) + " to spend.")
+
+Constitution = input ("How many build points do you want to assign to Constitution? ")
+if Build_Points == 0:
+    Constitution = "0"
+if int(Constitution) > 6:
+    Constitution = "6"
+if int(Constitution) < 0:
+    Constitution = "0"
+if int(Constitution) > Build_Points:
+    Constitution = str(Build_Points)
+Build_Points = (Build_Points - int(Constitution))
+print ("You Have " + str(Build_Points) + " to spend.")
+
+Intellegence = input ("How many build points do you want to assign to Intellegence? ")
+if Build_Points == 0:
+    Intellegence = "0"
+if int(Intellegence) > 6:
+    Intellegence = "6"
+if int(Intellegence) < 0:
+    Constitution = "0"
+if int(Intellegence) > Build_Points:
+    Intellegence = str(Build_Points)
+(Build_Points) = (Build_Points - int(Intellegence))
+print ("You Have " + str(Build_Points) + " to spend.")
+
+Wisdom = input ("How many build points do you want to assign to Wisdom? ")
+if Build_Points == 0:
+    Wisdom = "0"
+if int(Wisdom) > 6:
+    Wisdom = "6"
+if int(Wisdom) < 0:
+    Wisdom = "0"
+if int(Wisdom) > Build_Points:
+    Wisdom = str(Build_Points)
+Build_Points = (Build_Points - int(Wisdom))
+print ("You Have " + str(Build_Points) + " to spend.")
+
+Charisma = input ("How many build points do you want to assign to Charisma? ")
+if Build_Points == 0:
+    Charisma = "0"
+if int(Charisma) > 6:
+    Charisma = "6"
+if int(Charisma) < 0:
+    Charisma = "0"
+if int(Charisma) > int(Build_Points):
+    Charisma = str(Build_Points)
+Build_Points = (Build_Points - int(Charisma))
+print ("You Have " + str(Build_Points) + " to spend.")
+
+###############################
+# Print Ability scores to screen
+print ('\n')
+print ("STR: " + "+" + str(Strength))
+print ("DEX: " + "+" + str(Dexterity))
+print ("CON: " + "+" + str(Constitution))
+print ("INT: " + "+" + str(Intellegence))
+print ("WIS: " + "+" + str(Wisdom))
+print ("CHR: " + "+" + str(Charisma))
+
+###############################
+# Class Section
+print ('\n')
+print ("Choose a Class for your character:")
+print (" 1 Deathbringer")
+print (" 2 Grimscribe")
+print (" 3 Scoundrel")
+print (" 4 Witch Hunter")
+print (" 5 Plague Doctor")
+Choose_Class = input ("? ")
+
+if Choose_Class == "1":
+    Class = "Deathbringer"
+    HITPOINTS = random.randint(1, 10)
+    ClassFeature1 = "Hit Dice: d10. Use any armor or weapons."
+    ClassFeature2 = "Spend a Deathbringer Die to make one extra attack."
+    ClassFeature3 = "Second attack at 5th level."
+    ClassFeature4 = " "
+    ClassFeature5 = " "
+    Equipment_List = "waterskin, doublet, shield, 2 weapons"
+    
+if Choose_Class == "2":
+    Class = "Grimscribe"
+    HITPOINTS = random.randint(1, 6)
+    ClassFeature1 = "Hit Dice: d6. Cannot wear armor, simple weapons."
+    ClassFeature2 = "No spell slots. Roll to cast, DC 10."
+    ClassFeature3 = "On a roll of Natural 1 gain 1 Corruption and roll on the Miscast Table."
+    ClassFeature4 = "Gains spells by finding scrolls, spell books, or by having a friendly higher-level magic user teach them to you."
+    ClassFeature5 = "You are a living grimoire, must tattoo all spells on your skin."
+    Equipment_List = "waterskin, four cantrips, two first-level spells, dagger"
+
+if Choose_Class == "3":
+    Class = "Scoundrel"
+    HITPOINTS = random.randint(1, 8)
+    ClassFeature1 = "Hit Dice: d8. Light armor only, simple weapons."
+    ClassFeature2 = "Advantage when attempting stealth, climbing, listening, lockpicking, searching, etc."
+    ClassFeature3 = "+4 to hit and double damage from behind."
+    ClassFeature4 = " "
+    ClassFeature5 = " "
+    Equipment_List = "waterskin, leather jacket, club or dagger, thieves' tools, rope, grappling hook, lucky charm."
+
+if Choose_Class == "4":
+    Class = "Witch Hunter"
+    HITPOINTS = random.randint(1, 8)
+    ClassFeature1 = "Hit Dice: d8. Light armor, martial and simple weapons."
+    ClassFeature2 = "Cast Detect Evil and Protection from Evil 1x per day"
+    ClassFeature3 = "Turn Undead at will."
+    ClassFeature4 = " "
+    ClassFeature5 = " "
+    Equipment_List = "waterskin, leather coat, club, crossbow, holy symbol, 6 torches."
+    
+if Choose_Class == "5":
+    Class = "Plague Doctor"
+    HITPOINTS = random.randint(1, 6)
+    ClassFeature1 = "Hit Dice: d6. Leather armor only, simple weapons."
+    ClassFeature2 = "Cure Wounds once per patient per day, Cure Disease & Cure Poison 1x per day."
+    ClassFeature3 = "Create 1d4 potions a day (1) acid splash (2) sleep (3) poison spray (4) bomb [d10] (5) healing [d6] (6) hallucinations"
+    ClassFeature4 = " "
+    ClassFeature5 = " "
+    Equipment_List = "waterskin, Leather beak mask and coat, meat cleaver, medical kit, very suspicious diploma"
+
+###############################
+# Randomly chooses background
+Random_Backgrounds = ["Bounty Hunter", "Courtesan", "Duelist", "Executioner", "Farmer", "Grave Robber", "Leech Collector", "Mercenary", "Minor Noble", "Outlaw", "Performer", "Pit Fighter", "Pirate/Sailor", "Priest/nun/monk", "Rat Catcher", "Servant", "Student/Scholar", "Swineherd", "Soldier", "Urchin"]
+Background = random.choice(Random_Backgrounds)
+
+###############################
+# Randomly chooses misery
+Random_Misery = ["Abandoned at birth", "Banished from home", "Betrayed by a loved one", "Cursed by vengeful witch", "Disinherited or disowned", "Escaped bondage or prison", "Fled a scandal", "Framed for a crime", "Killed someone important", "Left for dead", "Locusts ate your crops", "Owe someone money", "Pursued by the law", "Raised in the streets", "Reduced to poverty", "Rejected by society", "Ruined by vice", "Suffering from amnesia", "Survived a massacre", "Town ravaged by plague"]
+Misery = random.choice(Random_Misery)
+
+###############################
+# Generate Starting gold and rations
+Gold = random.randint(1, 6)
+Rations = random.randint(1, 4)
+    
+###############################
+# Prints out the finished character to the screen
+print ('\n')
+Character_name = input ("What is your characters name? ")
+print ('\n')
+print ("Deathbringer Character Sheet 1.1")
+print ("------------------------------")
+print ("Character Name: " + Character_name)
+print ("Background: " + Background)
+print ("Misery: " + Misery)
+print ("Character Class: " + Class)
+print ("XP:    _____")
+print ("Level: _____")
+print ("------------------------------")
+print ("STR: " + "+" + str(Strength))
+print ("DEX: " + "+" + str(Dexterity))
+print ("CON: " + "+" + str(Constitution))
+print ("INT: " + "+" + str(Intellegence))
+print ("WIS: " + "+" + str(Wisdom))
+print ("CHR: " + "+" + str(Charisma))
+print ('\n')
+print ("Hit Points: " + str(HITPOINTS))
+print ('\n')
+print ("------------------------------")
+print (ClassFeature1)
+print (ClassFeature2)
+print (ClassFeature3)
+print (ClassFeature4)
+print (ClassFeature5)
+print ("------------------------------")
+print ("EQUIPMENT:")
+print (str(Gold) + " GP")
+print (str(Rations) + " days of rations")
+print (Equipment_List)
+
+###############################
+# Write character to a text file using the character name
+YN = input ("Do you want to save this character to a text file, Y or N? ")
+if YN == "N" or YN == "n":
+    exit()
+
+with open(Character_name + '.txt', 'w') as f:
+    f.write ('\n')
+    f.write ("Deathbringer Character Sheet 1.1" +'\n')
+    f.write ("------------------------------" +'\n')
+    f.write ("Character Name: " + Character_name +'\n')
+    f.write ("Background: " + Background +'\n')
+    f.write ("Misery: " + Misery +'\n')
+    f.write ("Character Class: " + Class +'\n')
+    f.write ("XP:    _____" +'\n')
+    f.write ("Level: _____" +'\n')
+    f.write ("------------------------------" +'\n')
+    f.write ("STR: " + "+" + str(Strength) +'\n')
+    f.write ("DEX: " + "+" + str(Dexterity) +'\n')
+    f.write ("CON: " + "+" + str(Constitution) +'\n')
+    f.write ("INT: " + "+" + str(Intellegence) +'\n')
+    f.write ("WIS: " + "+" + str(Wisdom) +'\n')
+    f.write ("CHR: " + "+" + str(Charisma) +'\n')
+    f.write  ('\n')
+    f.write ("Hit Points: " + str(HITPOINTS) +'\n')
+    f.write ('\n')
+    f.write ("------------------------------" +'\n')
+    f.write (ClassFeature1 +'\n')
+    f.write (ClassFeature2 +'\n')
+    f.write (ClassFeature3 +'\n')
+    f.write (ClassFeature4 +'\n')
+    f.write (ClassFeature5 +'\n')
+    f.write ("------------------------------" +'\n')
+    f.write ("EQUIPMENT:" +'\n')
+    f.write (str(Gold) + " GP" +'\n')
+    f.write (str(Rations) + " days of rations" +'\n')
+    f.write (Equipment_List +'\n')
+
+exit()
